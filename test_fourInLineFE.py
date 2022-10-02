c605ab2938d25fdd8e004adbf1f20ef777b98110
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2022-10-02 17:27:59 -0300

    2/10/2022

--- test_fourInLineFE.py
+++ test_fourInLineFE.py
@@ -14,20 +14,20 @@ class TestFourInLineFE(unittest.TestCase):
 
     def test_InputSad2(self):
         game = FourInLineFE()
-        with patch('builtins.input', return_value = 8):
+        with patch('builtins.input', return_value = 9):
             with patch('builtins.print') as patch_print:
                 game.play()
-                patch_print.assert_called_once_with("Ingrese un nùmero entre 0 y 7")
+                patch_print.assert_called_once_with("Ingrese un nùmero entre 1 y 8")
 
     def test_InputHappy(self):
         game = FourInLineFE()
         game.game.board[7][0] = 0
         game.game.board[6][0] = 0
         game.game.board[5][0] = 0
-        with patch('builtins.input', return_value = 0):
+        with patch('builtins.input', return_value = 1):
             with patch('builtins.print') as patch_print:
                 game.play()
-                patch_print.assert_any_call('¡Ganaste!')
+                patch_print.assert_any_call("¡Ganaste!")
 
 if __name__ == '__main__':
     unittest.main()
